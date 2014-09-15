7d037ae410415cba3840c8c521cef8260232da98
David <david.alonsobadia@gmail.com>
2014-09-15 15:23:25 +0200

    API Interface

---
+++ job_search_engine/src/com/searchengine/executables/APIInterface.java
@@ -0,0 +1,15 @@
+package com.searchengine.executables;
+
+public interface APIInterface {
+
+	void getAPIDataRequest();
+	
+	void setAPIRequest(String search);
+	
+	void getAPIResponse();
+	
+	void getAPIDataResponse();
+	
+	void setAPIResponse();
+	
+}
